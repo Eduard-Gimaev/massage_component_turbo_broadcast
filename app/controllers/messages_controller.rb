7c476adf7aaec83884ff98d18f96061ac6583653
GED <gimaev.ufa@gmail.com>
2025-03-12 16:24:27 +0100

    feat:added message component with turbo&&broadcast

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,16 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(body: params[:body])
+    if @message.save
+      Broadcast::Message.append(message: @message)
+      respond_to do |format|
+        format.html { redirect_to messages_path }
+        format.turbo_stream
+      end
+    end
+  end
+end
